d4057783eec972e8577c4b6183167f92ffd771f7
sushmadaware <88042519+sushmadaware@users.noreply.github.com>
2021-11-16 10:41:49 +0530

    Delete project directory

--- project/fapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns=[
-    path('signup/',signup_view,name='signup'),
-    path('adminview/', adminview, name='adminview'),
-    path('log_in/',login_view,name='log_in'),
-    path('log_out/',logout_view,name='log_out'),
-]
